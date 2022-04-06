fc2b6fc3b123cba4ae86d5b7111e6a70a07984dc
Bernardo Nogueira <62897976+Samuraiflamesf@users.noreply.github.com>
2022-04-06 11:54:35 -0300

    06/04/2022

--- index.php
+++ index.php
@@ -5,21 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paninel</title>
+    <link rel="stylesheet" href="style.css" />
 </head>
 <body>
-    <h2>Gerar senha de aterndimento</h2>
+    <h2 class='page-title'>GERAR SENHA DE ATENDIMENTO</h2>
+    <div class="wrapper">
+        <p class='page-subtitle'><strong>CIMEB</strong><br>
+        Centro de Infusões e Medicamentos Especializados da Bahia</p>
+        <div class="display"> <div id="time">12:00</div>
+        </div>
+    </div>
+    <span></span>
+    <span></span>
     <!--
         Chamar a função "gerarSenha" do Js para atendimento Dispensação
     -->
+    <div class='container'>
     <p><button type="button" onclick="gerarSenha(1)">Dispensação</button></p>
     <p><button type="button" onclick="gerarSenha(2)">Dispensação Preferencial</button></p>
         <!--
         Chamar a função "gerarSenha" do Js para atendimento Farmaceutico
     -->
-    <p><button type="button" onclick="gerarSenha(3)">Farmaceutico</button></p>
-    <p><button type="button" onclick="gerarSenha(4)">Farmaceutico Preferencial</button></p>
-
-
+    <p><button type="button" onclick="gerarSenha(3)">Farmacêutico</button></p>
+    <p><button type="button" onclick="gerarSenha(4)">Farmacêutico Preferencial</button></p>
+    </div>
+    
+    <p>2022 - @BernardoNogueira8</p>
     <script src="js/custom.js"></script>
 </body>
 </html>
